1614065541d3e90fdcfb4d9507f88a2cb69b7b53
NDAYISENGAManu <webmasterio2015@gmail.com>
2022-09-01 09:19:02 +0200

    profile and repos added

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,27 @@ import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const {githubUser} = React.useContext(GithubContext)
+  const {avatar_url, html_url, name, company, blog, bio, location, twitter_username} = githubUser;
+
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} />
+        <div> 
+          <h4>{name}</h4>
+          <p>@{twitter_username || 'No twitter account'}</p>
+        </div>
+        <a href={html_url}>follow</a>
+      </header>
+      <p className='bio'>{bio}</p>
+      <div className='links'>
+        <p><MdBusiness /> {company}</p>
+        <p><MdLocationOn /> {location || 'earth'}</p>
+        <a href={`https://${blog}`}> <MdLink /> {blog || 'have fun with code'}</a>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
@@ -13,7 +33,7 @@ const Wrapper = styled.article`
   border-bottom-right-radius: var(--radius);
   position: relative;
   &::before {
-    content: 'user';
+    content: 'Card';
     position: absolute;
     top: 0;
     left: 0;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -2,11 +2,96 @@ import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
-const Repos = () => {
-  return <h2>repos component</h2>;
-};
 
-const Wrapper = styled.div`
+const Repos = () => {
+  const { repos } = React.useContext(GithubContext)
+
+  // console.log(repos)
+
+  // ========== this were returning the languages % ========= //
+
+  // let languages = repos.reduce((total, item)=>{
+  //   const { language } = item;
+  //   if(!language) return total;
+  //   if (!total[language]) {
+  //     total[language] = { label:language, value:1 };
+  //   } else {
+  //     total[language] = {
+  //       ...total[language], 
+  //       value: total[language].value + 1,
+  //     };      
+  //   }
+  //   return total;
+  // },{});
+
+  //sort the items
+  // languages = Object.values(languages).sort((a,b)=>{
+  //   return b.value - a.value;
+  // }).slice(0, 5);
+
+  const languages = repos.reduce((total, item)=>{
+    const { language, stargazers_count } = item;
+    if(!language) return total;
+    if (!total[language]) {
+      total[language] = { label:language, value:1, stars:stargazers_count };
+    } else {
+      total[language] = {
+        ...total[language], 
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      };      
+    }
+    return total;
+  },{});
+
+
+  //sort the items
+  const mostUsed = Object.values( languages ).sort(( a, b ) => {
+    return b.value - a.value;
+  }).slice(0, 5);
+
+  //most popular language and sorted baed on stars
+
+  const mostPopular = Object.values(languages).sort((a,b) => {
+    return b.stars - a.stars;
+  }).map((item)=>{
+    return {...item, value:item.stars}
+  }).slice(0,5);
+
+
+  const chartData = [
+    {
+      label: 'HTML',
+      value: "17"
+    },
+    {
+      label: "CSS",
+      value: "23"
+    },
+    {
+      label: "Javascript",
+      value: "61"
+    },
+  ];
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <Pie3D data={chartData} />  {/* <ExampleChart data={chartData} /> */}
+        {/* <div></div> */}
+        <Doughnut2D data={mostPopular} />
+        <div></div>
+        {/* <Column3D data={chartData} /> */}
+        {/* <Bar3D data={chartData} /> */}
+        {/* <div></div> */}
+        {/* <ExampleChart data={chartData} /> */}
+        <div></div>
+      </Wrapper>
+    </section>
+  )
+}; 
+ 
+const Wrapper = styled.div` 
   display: grid;
   justify-items: center;
   gap: 2rem;
