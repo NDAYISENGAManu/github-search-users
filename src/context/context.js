6100ab466126fd86e5dc884035a94af5932b3847
NDAYISENGAManu <webmasterio2015@gmail.com>
2022-09-28 10:41:18 +0200

    the complete search

--- src/context/context.js
+++ src/context/context.js
@@ -19,12 +19,60 @@ const GithubProvider = ({children}) => {
 
     //request loading
     const [requests, setRequests] = useState(0);
-    const [loading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     //errors
     const [error, setError] = useState({show: false, msg:''})
     //search github user
     const searchGithubUser = async (user) => {
-        console.log(user);
+        toggleError();  // to remove the error msg once new search load
+        // console.log(user);
+        setIsLoading(true);
+        const response = await axios(`${rootUrl}/users/${user}`).
+        catch (err => 
+            console.log(err)
+        );
+        
+        if(response){
+            setGithubUser(response.data);
+            //more logic here
+            //binding the searched user to the main repos
+            const {login, followers_url} = response.data;
+
+            //repos
+            axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) => 
+                setRepos(response.data)
+            );
+            //followers
+            axios(`${followers_url}?per_page=100`).then((response) => 
+                setFollowers(response.data)
+            );
+
+            //we need all the data to show up at the same time
+            //that's why we are going to add await on every axios and set promise          
+
+            // await Promise.allSettled([axios(`${rootUrl}/users/${login}/repos?per_page=100`), axios(`${followers_url}?per_page=100`) ])
+
+            await Promise.allSettled([
+                axios(`${rootUrl}/users/${login}/repos?per_page=100`), 
+                axios(`${followers_url}?per_page=100`) 
+            ]).then((results)=>{
+                // console.log(results);
+                const [repos, followers] = results;
+                const status = 'fulfilled';
+                if(repos.status === status){
+                    setRepos(repos.value.data);
+                }
+                if(followers.status === status){
+                    setFollowers(followers.value.data);
+                }
+            }).catch(err => console.log(err));
+
+        }
+        else{
+            toggleError(true, 'There is no user with that username');
+        }
+        checkRequests();
+        setIsLoading(false)
     };
     // check rate
     const checkRequests = () => {
@@ -48,7 +96,7 @@ const GithubProvider = ({children}) => {
     useEffect(checkRequests, []);
 
     return (
-        <GithubContext.Provider value={{ githubUser, repos, followers, requests, error }}>{children}</GithubContext.Provider>
+        <GithubContext.Provider value={{ githubUser, repos, followers, requests, error, searchGithubUser, isLoading }}>{children}</GithubContext.Provider>
     );
 };
 
